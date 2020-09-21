6987f1e813eaf636e7741a010674de1d6bd65c91
Isar Ramses <rammses_@live.com.mx>
2020-09-20 20:53:23 -0500

    add mongo

--- README.md
+++ README.md
@@ -2,11 +2,27 @@
 
 A simple backend using Deno, GraphQL and MongoDB.
 
-> Recommendation: `Ctrl + P` and paste `ext install denoland.vscode-deno` in order to gain intellisense support, intelligent module import and more features. You can disable Deno's formatting tools in .vscode/settings.json
+> Recommendation (VS Code): `Ctrl + P` and paste `ext install denoland.vscode-deno` in order to gain intellisense support, intelligent module import and more features.
 
 ---
 
 ## How to run
+
+### MongoDB
+
+You can use Docker to prepare a Mongo image using the following command the first time
+
+```sh
+docker run -v ~/docker/mongo_test/data:/data/db -p 27017:27017 --name mongo_test -d mongo:4.2.9-bionic
+```
+
+and the following times this other
+
+```sh
+docker start mongo_test
+```
+
+### Deno
 
 ```sh
 deno run --allow-net --allow-write --allow-read --allow-plugin --unstable server.ts
--- server.ts
+++ server.ts
@@ -6,58 +6,96 @@
  */
 
 // 'https://deno.land/x/oak@v6.0.1/mod.ts'
-import {
-  Application,
-  Router,
-  RouterContext,
-} from "https://deno.land/x/oak/mod.ts";
+import { Application, Router, RouterContext } from 'https://deno.land/x/oak/mod.ts'
 // 'https://x.nest.land/oak-graphql@0.5.9/mod.ts'
-import { applyGraphQL, gql } from "https://deno.land/x/oak_graphql/mod.ts";
+import { applyGraphQL, gql } from 'https://deno.land/x/oak_graphql/mod.ts'
+// 'https://deno.land/x/mongo@v0.12.1/mod.ts'
+import { MongoClient, ObjectId } from 'https://deno.land/x/mongo/mod.ts'
+import { config } from 'https://deno.land/x/dotenv/mod.ts'
 
-const app = new Application();
+const { PORT, MONGO_URI } = config()
+
+const app = new Application()
+
+// MongoDB
+const client = new MongoClient()
+client.connectWithUri('mongodb://localhost:27017')
+const db = client.database('deno_graphql')
+const users = db.collection('users')
 
 const types = gql`
   type User {
+    id: ID!
+    username: String!
     firstName: String
     lastName: String
   }
 
   input UserInput {
+    username: String
     firstName: String
     lastName: String
   }
 
-  type Query {
-    getUser(id: String): User
+  type UpdateOutput {
+    matchedCount: Int
+    modifiedCount: Int
   }
-`;
+
+  type DeleteOutput {
+    deleteCount: Int
+  }
+
+  type Query {
+    getOneUser(id: String): User
+  }
+
+  type Mutation {
+    addOneUser(data: UserInput!): User
+    updateOneUser(data: UserInput, id: String!): UpdateOutput
+    deleteOneUser(id: String!): DeleteOutput
+  }
+`
 
 const resolvers = {
   Query: {
-    getUser: async (
-      parent: any,
-      { id }: { id: string },
-      context: any,
-      info: any,
-    ) => {
-      return {
-        firstName: "dummy firstName",
-        lastName: "dummy lastName",
-      };
-    },
+    getOneUser: async (parent: any, args: any, context: any, info: any) => {
+      return await users.findOne({ _id: ObjectId(args.id) })
+    }
   },
-};
+
+  Mutation: {
+    addOneUser: async (parent: any, args: any, context: any, info: any) => {
+      const { $oid: id } = await users.insertOne(args.data)
+
+      return { id }
+    },
+    updateOneUser: async (parent: any, args: any, context: any, info: any) => {
+      const { matchedCount, modifiedCount } = await users.updateOne(
+        { _id: ObjectId(args.id) },
+        { $set: args.data }
+      )
+
+      return { matchedCount, modifiedCount }
+    },
+    deleteOneUser: async (parent: any, args: any, context: any, info: any) => {
+      return {
+        deleteCount: await users.deleteOne({ _id: ObjectId(args.id) })
+      }
+    }
+  }
+}
 
 const GraphQLService = await applyGraphQL<Router>({
   Router,
   typeDefs: types,
   resolvers: resolvers,
   context: (ctx: RouterContext) => {
-    return { advice: "This context is accessible to every resolvers." };
-  },
-});
+    return { advice: 'This context is accessible to every resolvers.' }
+  }
+})
 
-app.use(GraphQLService.routes(), GraphQLService.allowedMethods());
+app.use(GraphQLService.routes(), GraphQLService.allowedMethods())
 
-console.log("GraphQL Playground at http://localhost:8000/graphql");
-await app.listen({ port: 8000 });
+console.log(`GraphQL Playground at http://localhost:${PORT}/graphql`)
+await app.listen({ port: Number(PORT) })
